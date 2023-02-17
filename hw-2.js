c6162c3caa334f54ef29e8286e255b21b55b91ce
Olya192 <Olya192@bk.ru>
2023-02-17 21:47:27 +0400

    выполнила ДЗ урока 2.2 по JS

---
+++ hw-2.js
@@ -0,0 +1,67 @@
+// let a = 10;
+// alert (a);
+// a=20;
+// alert (a);
+
+
+
+// let release = 2007;
+// alert (`год выпуска первого iPhone ${release}`);
+
+
+
+// let name = "Brendon Eich";
+// alert (`имя создателя javascript ${name}`);
+
+
+
+// let a = 10;
+// let b = 2;
+// alert (`a+b=${a+b}`);
+// alert (`a-b=${a-b}`);
+// alert (`a*b=${a*b}`);
+// alert (`a/b=${a/b}`);
+
+
+
+// let a = 2;
+// let result = a**5;
+// alert (result);
+
+
+
+// let a = 9;
+// let b = 2;
+// alert (a%b);
+
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
+
+
+
+// let age = prompt ("Сколько вам лет?");
+// alert (age);
+
+
+
+// let user = {
+//     name: "Ola",
+//     age: 30,
+//     isAdmin: true
+// };
+// user["city of residence"] = "Samara";
+// user.age = 29;
+// delete user["city of residence"];
+// let info = prompt("Какую информацию хотите узнать о пользователе?");
+// alert(user[info]);
+
+
+
+// let name = prompt ("Введите свое имя");
+// alert (`Привет, ${name}!`)
