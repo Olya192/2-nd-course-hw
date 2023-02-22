aef896c3411712986372be024d179cd6989b9081
Olya192 <Olya192@bk.ru>
2023-02-22 20:54:52 +0400

    Сделала основное задание по JS

---
+++ hw-3.js
@@ -0,0 +1,67 @@
+// const password = 'пароль';
+// let answerPass = prompt ('Введите пароль');
+// answerPass = answerPass.toLocaleLowerCase();
+
+// if (password=answerPass) { alert ('Пароль введен верно')
+
+// } else { alert ('Пароль введен неправильно')
+
+// }
+
+
+// let c = prompt ('Введите число');
+
+// if (c>0 && c<10) { alert ('Верно')
+
+// } else { alert ('Неверно')
+
+// }
+
+
+
+// const d = 150;
+// const e = 90;
+
+// if (d>100 || e>100) { alert ('Верно')
+
+// } else { alert ('Неверно')
+
+// }
+
+
+
+// let a = '2';
+// let b = '3';
+// a = Number (a);
+// b = Number (b);
+// alert(a + b);
+
+
+
+// let monthNumber = prompt('Введите номер месяца');
+// monthNumber = Number(monthNumber);
+
+// switch (monthNumber) {
+//     case 1:
+//     case 2:
+//     case 12:
+//         console.log('Зима');
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log('Весна');
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log('Лето');
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log('Осень');
+//         break;
+//     default: console.log('Введено не корректное число');
+//         break;
+// }
