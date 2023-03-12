fb3bcb4bf11b89cfe41f803e9d1563a37ee06d5f
Olya192 <Olya192@bk.ru>
2023-03-12 22:20:31 +0400

    reduce опробован

--- Scripts/hw-8.js
+++ Scripts/hw-8.js
@@ -15,16 +15,20 @@
 //     return result
 // }
 
-// function sum(array) {
-//     let result = 0;
-//     for (let index = 0; index < array.length; index++) {
-//         result += array[index];
-//     }
 
-//     return result
+// function sum(array) {
+//     // let result = 0;
+//     // for (let index = 0; index < array.length; index++) {
+//     //     result += array[index];
+//     // }
+// return array.reduce(function (currentSum, currentNumber) {
+//     return currentSum + currentNumber
+//   }, 0)
+
+//     // return result
 // }
 
-// console.log(getResult([3, 4, 1, 9], mult));
+// // console.log(getResult([3, 4, 1, 9], mult));
 // console.log(getResult([3, 4, 1, 9], sum));
 
 
